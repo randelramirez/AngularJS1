97db3aaaf3998ab28fef1c390843b9cdf20dfef2
Randel Ramirez <randel1_23@hotmail.com>
2014-06-30 19:01:40 +0800

    Added cache, compile, filter service

---
+++ AngularJS1/Scripts/js/controllers/CompileSampleController.js
@@ -0,0 +1,9 @@
+﻿'use strict'
+
+eventsApp.controller('CompileSampleController',
+    function CompileSampleController($scope, $compile) {
+        $scope.appendDivToElement = function (markup) {
+            console.log("markup",markup);
+            return $compile(markup) ($scope).appendTo(angular.element("#appendHere"))
+        }
+    });
--- AngularJS1/Scripts/js/controllers/EditEventController.js
+++ AngularJS1/Scripts/js/controllers/EditEventController.js
@@ -3,8 +3,9 @@
 eventsApp.controller('EditEventController',
     function EditEventController($scope, eventData) {
 
-        // no need to declare event object, ng-model will create it in the view ng-model="event.myProperty"
-       // $scope.event = {};
+        // no need to declare event object, ng-model will create it in the view ng-model="event.myProperty", *for create*
+        // for editing use the code below *for edit*
+        //$scope.event = eventData.getEvent();
 
         $scope.saveEvent = function (event, form) {
             if (form.$valid) {
