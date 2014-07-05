8cd7e77fdd27b20ba1d509a97bbb22b1ae302ce6
Randel Ramirez <randel1_23@hotmail.com>
2014-07-06 00:17:18 +0800

    using isolated scope

--- AngularJS1/Scripts/singlepagejs/directives/eventThumbnail.js
+++ AngularJS1/Scripts/singlepagejs/directives/eventThumbnail.js
@@ -7,6 +7,11 @@ eventsApp.directive('eventThumbnail', function ($compile) {
         // replace the html instead of appending it to, now instead of having <event-thumbnail>//html content of template here</event-thumbnail>
         // it's going to add the html of the template without the parent element <event-thumbnail>
         replace: true,
-        templateUrl: rootLocation +'/Templates/Directives/EventThumbnail.html'
+        templateUrl: rootLocation + '/Templates/Directives/EventThumbnail.html',
+        scope: {
+            event: "=event"
+            // or
+            // event: "=", because event mathces event="event", the attr name is also event
+        }
     }
 })
