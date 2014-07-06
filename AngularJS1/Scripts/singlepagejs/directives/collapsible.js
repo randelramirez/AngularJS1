0dd743cfa1f40a68a654466dfd8dc528c2bb4277
Randel Ramirez <randel1_23@hotmail.com>
2014-07-06 18:43:16 +0800

    Event details using directives for session thumnails, transclusion for details of the session, hide/show on click

--- AngularJS1/Scripts/singlepagejs/directives/collapsible.js
+++ AngularJS1/Scripts/singlepagejs/directives/collapsible.js
@@ -4,10 +4,18 @@ eventsApp.directive('collapsible', function () {
     return {
         restrict: 'E',
         replace: true,
-        template: '<div><h4 class="well-title">{{title}}</h4><div>',
+        transclude: true,
+        template: '<div><h4 class="well-title" ng-click="toggleVisibility()">{{title}}</h4>'
+            + '<div ng-show="visible" ng-transclude></div> <div>',
+        controller: function($scope){
+            $scope.visible = false;
+
+            $scope.toggleVisibility = function () {
+                $scope.visible = !$scope.visible;
+            }
+        },
         scope: {
             title: '@'
         }
     }
-
 });
