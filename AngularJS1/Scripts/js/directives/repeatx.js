7447adc2ae6bc51458421f9a9af7a1752a6e75fc
Randel Ramirez <randel1_23@hotmail.com>
2014-07-06 23:03:16 +0800

    Additional files included

---
+++ AngularJS1/Scripts/js/directives/repeatx.js
@@ -0,0 +1,53 @@
+﻿'use strict';
+
+eventsApp.directive('repeatX', function () {
+   
+
+    //return {
+    //    link: function (scope, element, attributes, controller) {
+    //        // without number would also work, 
+    //        // Number() is js a function, like ParseInt
+    //        for (var i = 0; i < Number(attributes.repeatX) - 1; i++) {
+    //            //append the element, the element.clone() will clone the element and then append it 
+    //            element.after(element.clone());
+    //        }
+    //    }
+    //}
+
+    // make sure $compile is injected in the constructor of this directive
+    // using $compile service
+    // call to $compile()(scope) is expensive, it traverses the dom and look for directives that need to be processed
+    //return {
+    //    link: function (scope, element, attributes, controller) {
+    //        for (var i = 0; i < Number(attributes.repeatX) - 1; i++) {
+    //            element.after($compile(element.clone().attr('repeat-x', 0))(scope));
+    //        }
+    //    }
+    //};
+
+    // using compile function
+    // scope is not available => all about dom manipulations, not bindings into the scope
+    //return {
+    //    compile: function (element, attributes) {
+    //        for (var i = 0; i < Number(attributes.repeatX) - 1; i++) {
+    //            element.after(element.clone().attr('repeat-x', 0));
+    //        }
+    //    }
+    //};
+
+    // directive function to use when mainipulating the dom is compile
+    return {
+        compile: function (element, attributes) {
+            for (var i = 0; i < Number(attributes.repeatX) - 1; i++) {
+                element.after(element.clone().attr('repeat-x', 0));
+            }
+            return function (scope,element, attributes, controller) {
+                attributes.$observe('text', function (newValue) {
+                    if (newValue === 'Hello World') {
+                        element.css('color', 'red');
+                    }
+                })
+            }
+        }
+    };
+});
