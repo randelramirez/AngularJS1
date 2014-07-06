d4e324ab6ac1cda0f4f805fa7ac3830e21d6d94c
Randel Ramirez <randel1_23@hotmail.com>
2014-07-06 22:56:40 +0800

    finished the directives module

--- AngularJS1/Scripts/js/directives/greetingwithrequire.js
+++ AngularJS1/Scripts/js/directives/greetingwithrequire.js
@@ -6,6 +6,7 @@ eventsApp.directive('greeting', function () {
         restrict: 'E',
         replace: true,
         template: "<button class='btn' ng-click='sayHello()'>Say Hello</button>",
+        transclude: true,
         //priority: 2, higher priority means it will execute first
         //terminal: true, terminal means that directives with less than 2 priority will not be executed
         // note about terminal, it also affects built-in angular directives like ng-click
@@ -24,7 +25,14 @@ eventsApp.directive('greeting', function () {
 .directive('finnish', function () {
     return {
         restrict: 'A',
-        require: 'greeting', // this directive requires a another directive named greeting
+        //require: 'greeting', // this directive requires a another directive named greeting
+
+        //using transclusion with nested directives
+        // note on require, it means that it requires the same directive in the same element 
+        // the code below will not work, greeting directive is in the parent element
+        // <greeting><div finnish korean></div></greeting> // note restrict is A, so the div will use the directives as attributes
+        require: "^greeting",
+       
         link: function (scope, element, attrs, controller) {
             controller.addGreeting('hei');
         }
@@ -33,7 +41,9 @@ eventsApp.directive('greeting', function () {
 .directive('korean', function () {
     return {
         restrict: 'A',
-        require: 'greeting', // this directive requires a another directive named greeting
+        //require: 'greeting', // this directive requires a another directive named greeting
+
+        require: "^greeting",
         link: function (scope, element, attrs, controller) {
             controller.addGreeting('annyeong-hasseyo');
         }
